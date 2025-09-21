70c78597236142ea76239a47566ff79d5f262461
ryanslee6 <ryan.s.lee6@gmail.com>
2025-09-21 13:44:54 -0700

    player model added

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import pygame
 from constants import *
+from player import *
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
-
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     running = True
     while running:
@@ -15,8 +17,9 @@ def main():
                 return
         
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
-    
+        dt = clock.tick(60)
     
 
     
