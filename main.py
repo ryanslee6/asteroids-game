dc864fbea2b234da10c03a9e0c9e33c1d34d4b00
ryanslee6 <ryan.s.lee6@gmail.com>
2025-09-25 13:56:50 -0700

    End game screen and high score system added. Projectile also made more lazer like

--- main.py
+++ main.py
@@ -1,11 +1,85 @@
 import pygame
 import sys
+import os
 from constants import *
 from player import *
 from asteroid import *
 from asteroidfield import *
 
+def load_high_score():
+    if not os.path.exists(HIGH_SCORE_FILE):
+        return 0
+    with open(HIGH_SCORE_FILE, "r") as f:
+        try:
+            return int(f.read())
+        except ValueError:
+            return 0
+        
+def save_high_score(score):
+    high_score = load_high_score()
+    if score > high_score:
+        with open(HIGH_SCORE_FILE, "w") as f:
+            f.write(str(score))
 
+def reset_game(player, asteroids, shots, particles, updatable, drawable):
+
+    score = 0
+
+    player.position = pygame.Vector2(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+    player.rotation = 0
+
+    for group in [asteroids, shots, particles, updatable, drawable]:
+        group.empty()
+
+    updatable.add(player)
+    drawable.add(player)
+
+    asteroid_field = AsteroidField()
+
+    return score
+
+
+def end_game_screen(screen, player, asteroids, shots, particles, updatable, drawable, score):
+    save_high_score(score)
+    high_score = load_high_score()
+
+
+    #score = reset_game(player, asteroids, shots, particles, updatable, drawable)
+
+    font = pygame.font.SysFont(None, 50)
+    small_font = pygame.font.SysFont(None, 36)
+
+    running = True
+    while running:
+        screen.fill((0, 0, 0))
+        
+
+        text = font.render("Game Over!", True, (255, 0, 0))
+        screen.blit(text, (screen.get_width()//2 - text.get_width()//2, 100))
+
+        score_text = small_font.render(f"Your Score: {score}", True, (255, 255, 255))
+        screen.blit(score_text, (screen.get_width()//2 - score_text.get_width()//2, 200))
+
+        high_text = small_font.render(f"High Score: {high_score}", True, (0, 255, 0))
+        screen.blit(high_text, (screen.get_width()//2 - high_text.get_width()//2, 250))
+
+        restart_text = small_font.render(f"Press r to Restart or q to Quit", True, (255, 255, 255))
+        screen.blit(restart_text, (screen.get_width()//2 - restart_text.get_width()//2, 350))
+
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    pygame.quit()
+                    sys.exit()
+                elif event.key == pygame.K_r:
+                    running = False
+                    score = reset_game(player, asteroids, shots, particles, updatable, drawable)
+                    return score
 
 def main():
     pygame.init()
@@ -43,6 +117,7 @@ def main():
             elif event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_p:
                     paused = not paused
+                
         
         screen.blit(background, (0, 0))
         
@@ -63,9 +138,8 @@ def main():
 
         for asteroid in asteroids:
             if player.collides_with(asteroid):
-                print("Game over!")
-                pygame.quit()
-                sys.exit()
+                #end_game_screen(screen, score)
+                score = end_game_screen(screen, player, asteroids, shots, particles, updatable, drawable, score)
 
         for asteroid in asteroids:
             for shot in shots:
@@ -74,6 +148,9 @@ def main():
                     asteroid.explode()
                     asteroid.split()
                     score += 1
+
+        #save_high_score(score)
+        #high_score = load_high_score
 
         fps_text = font.render(f"FPS: {int(clock.get_fps())}", True, (255, 255, 255))
         screen.blit(fps_text, (10, 10))
--- player.py
+++ player.py
@@ -56,28 +56,70 @@ class Player(CircleShape):
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
-        direction = pygame.Vector2(0, 1)
-        direction = direction.rotate(self.rotation)
+        direction = pygame.Vector2(0, 1).rotate(self.rotation)
         velocity = direction * PLAYER_SHOT_SPEED
+        position = pygame.Vector2(self.position.x, self.position.y)
+        Shot(position, velocity)
 
-        Shot(self.position.x, self.position.y, SHOT_RADIUS, velocity)
         self.shoot_timer = PLAYER_SHOOT_COOLDOWN
 
+#    def shoot(self):
+#        direction = pygame.Vector2(0, 1)
+#        direction = direction.rotate(self.rotation)
+#        velocity = direction * PLAYER_SHOT_SPEED
+#
+#        Shot(self.position.x, self.position.y, SHOT_RADIUS, velocity)
+#        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
 
-class Shot(CircleShape):
-    def __init__(self, x, y, radius, velocity):
-        super().__init__(x, y, radius)
-        pygame.sprite.Sprite.__init__(self, self.containers)
-        self.velocity = velocity
-
-    def draw(self, screen):
-        pygame.draw.circle(
-            screen,
-            "white",
-            (int(self.position.x), int(self.position.y)),
-            self.radius,
-            0
-        )
+class Shot(pygame.sprite.Sprite):
+    def __init__(self, position, velocity, color=(0, 255, 0)):
+        super().__init__(self.containers)
+        self.position = pygame.Vector2(position)
+        self.velocity = pygame.Vector2(velocity)
+        self.color = color
+        self.length = 16
+        self.width = 3
+        self.radius = 2
 
     def update(self, dt):
         self.position += self.velocity * dt
+    
+    def draw(self, screen):
+        end_pos = self.position + self.velocity.normalize() * self.length
+        
+        glow_surf = pygame.Surface((screen.get_width(), screen.get_height()), pygame.SRCALPHA)
+        for i, alpha in enumerate([50, 30, 15], start = 1):
+            pygame.draw.line(
+                glow_surf,
+                (*self.color, alpha),
+                self.position,
+                end_pos,
+                self.width + i*2
+            )
+        screen.blit(glow_surf, (0, 0))
+        
+        pygame.draw.line(screen, self.color, self.position, end_pos, self.width)
+
+    def collides_with(self, other):
+        distance = self.position.distance_to(other.position)
+        return distance <= (self.radius + other.radius)
+
+
+
+#class Shot(CircleShape):
+#    def __init__(self, x, y, radius, velocity):
+#        super().__init__(x, y, radius)
+#        pygame.sprite.Sprite.__init__(self, self.containers)
+#        self.velocity = velocity
+#
+#    def draw(self, screen):
+#        pygame.draw.circle(
+#            screen,
+#            "white",
+#            (int(self.position.x), int(self.position.y)),
+#            self.radius,
+#            0
+#        )
+#
+#    def update(self, dt):
+#        self.position += self.velocity * dt
